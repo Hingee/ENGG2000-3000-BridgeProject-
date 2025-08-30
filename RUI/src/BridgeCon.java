48611ac2d736e24cf8573dcad7e79351fd4944fe
Nathan Hingee <nathan.hingee@students.mq.edu.au>
2025-08-30 21:26:12 +1000

    Added basic network functionality

--- RUI/src/BridgeCon.java
+++ RUI/src/BridgeCon.java
@@ -1,5 +1,69 @@
 package RUI.src;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BridgeCon {
-    
+    private int port;
+    private boolean conStatus;
+    private Queue<String> msgs;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private Thread listerThread;
+
+    // Setup input and output streams for communication with the client
+    private BufferedReader in;
+    private PrintWriter out;
+
+    BridgeCon() {
+        port = 9090;
+        conStatus = false;
+        msgs = new LinkedList<String>();
+    }
+
+    public void startUp() throws IOException{
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server is running and waiting for client connection...");
+
+        clientSocket = serverSocket.accept();
+        System.out.println("Client connected!");
+
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+    }
+
+    public void recieve() {
+        String msg = in.readLine();
+        System.out.println("Recieved: "+msg+" from port: " + port);
+        msgs.add(msg);
+    }
+
+    public void send(String msg) throws IOException{
+        out.println(msg);
+        System.out.println("Sent: "+msg+" from port: " + port);
+    }
+
+    synchronized public String pollLastMsg() {
+        return msgs.poll();
+    }
+
+    public void close() throws IOException{
+        clientSocket.close();
+        serverSocket.close();
+        System.out.println("Closed Connection");
+    }
+
+    public boolean getConStatus() {
+        return conStatus;
+    }
+
+    public void setConStatus(boolean b) {
+        conStatus = b;
+    }
 }
--- RUI/src/BridgeData.java
+++ RUI/src/BridgeData.java
@@ -1,5 +1,42 @@
 package RUI.src;
 
 public class BridgeData {
-    
+    private String BridgeState;
+    private String gateState;
+    private boolean isAlarmOn;
+    private char trafficLights;
+    private char boatLights;
+
+    BridgeData() {
+        BridgeState = "";
+        gateState = "";
+        isAlarmOn = false;
+        trafficLights = '.';
+        boatLights = '.';
+    }
+
+    public String getSTAT() {
+        String a = "OFF";
+        if(isAlarmOn) a = "ON";
+        return "STAT "+BridgeState+" "+gateState+" "+a+" "+trafficLights+" "+boatLights;
+    }
+
+    public boolean update(String stat) {
+        if(!isValid(stat)) return false;
+
+        String[] wordsArray = stat.split("\s+");
+        BridgeState = wordsArray[1];
+        gateState = wordsArray[2];
+        if(wordsArray[3].equals("ON")) isAlarmOn = false;
+        else isAlarmOn = false;
+        trafficLights = wordsArray[4].charAt(0);
+        boatLights = wordsArray[5].charAt(0);
+
+        return true;
+    }
+
+    private boolean isValid(String stat) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isValid'");
+    }
 }
