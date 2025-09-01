feaf2e694e2687aebb097945799c4c488365901a
Nathan Hingee <nathan.hingee@students.mq.edu.au>
2025-09-01 13:23:05 +1000

    Adjusted Code

--- Esp32/src/main.ino
+++ Esp32/src/main.ino
@@ -1,16 +1,17 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <iostream>
-#include <string.h>
 #include <Arduino.h>
-using namespace std;
 
-// WiFi credentials/Net
-const char* ssid = "ENGG2K3K";  //Todo change needed
-const char* pass = "YOUR_PASSWORD";  //Todo change needed
-const char* serverIp = "YOUR_SERVER_IP";  //Todo change needed
-unsigned int serverPort = 9999;  //Todo change needed
+// WiFi credentials
+const char* ssid = "ENGG2K3K";
+const char* pass = "YOUR_PASSWORD";
+const char* serverIp = "YOUR_SERVER_IP";
+unsigned int serverPort = 9999;
 WiFiClient client;
+
+// Forward declarations
+void send(String msg);
+String receive();
 
 // Task function
 void core0(void *parameter) {
@@ -23,32 +24,31 @@ void core0(void *parameter) {
       } else {
         Serial.println("Connection failed.");
         delay(1000);
-        continue; // Retry connection
+        continue;
       }
     }
 
     send("Hello");
-    String msg = recieve();
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // Sleep 1s
+    String msg = receive();
+    vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
-
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Connecting to WiFi...");
+    delay(500);
   }
   Serial.println("Connected to WiFi");
 
-  // Create tasks pinned to specific cores
-  xTaskCreatePinnedToCore(core0, "Core0", 2048, NULL, 1, NULL, 0);
+  // Create task pinned to Core 0
+  xTaskCreatePinnedToCore(core0, "Core0", 4096, NULL, 1, NULL, 0);
 }
 
 void loop() {
-
-  delay(5000); // Wait before sending again
+  delay(5000);
 }
 
 void send(String msg) {
@@ -57,11 +57,12 @@ void send(String msg) {
   Serial.println(msg);
 }
 
-String recieve() {
+String receive() {
   if (client.available()) {
     String line = client.readStringUntil('\n');
     Serial.print("Received: ");
     Serial.println(line);
+    return line;
   }
-  return line;
+  return ".";
 }
