4a15203116b94b56e1f0d6ae54d6654893c917f1
EJ2503 <elijah.johnson@students.mq.edu.au>
2025-10-01 11:59:41 +1000

    Bugfixes & print messages
    This version compiles fine, although it hasn't been tested with the physical hardware yet

--- src/BridgeController/BridgeSync/BridgeSync.cpp
+++ src/BridgeController/BridgeSync/BridgeSync.cpp
@@ -5,30 +5,28 @@
 //When the motor is hitting its limit of bridge opening, shut off its power. (this could be changed to be implemented into the RUI if needed)
 
 #include <Arduino.h>
-#include <string>
-
-
-//Run Types
-std::string rSC = "runSyncCheck"; //Main
-std::string rMC = "runMotorClockwise"; //Run continuously clockwise
-std::string rMAC = "runMotorAntiClockwise"; //Run continuously anticlockwise
 
 //System Run Type (MANUAL OVERRIDE) 
-std::string runType = rSC;
+int runType = 0;
+    //0 = Run sync check (Main)
+    //1 = Run continuously clockwise
+    //2 = Run continuously anticlockwise
 
 //Motor specific
 static int motorDriverPin1 = 27; 
 static int motorDriverPin2 = 26; 
 int duration = 5000; //1000 is a second
-int totalRotations = 0; 
+float totalRotations = 0.0; 
 
 //Encoder
-const int encoderPinA = 34;
+const int encoderPinA = 34; //encoder won't be needed on main, only needed for trouble shooting
 volatile unsigned long pulseCount = 0;
-const int pulsesPerRevolution = 700; //change this in main
+const int pulsesPerRevolution = 700;
 
 
-void IRAM_ATTR onPulse();
+void IRAM_ATTR onPulse() { //change in main
+    pulseCount++;
+}
 
 void setup() {
     Serial.begin(115200);
@@ -44,22 +42,17 @@ void setup() {
 }
 
 void loop() {
-    switch (runType) {
-    case ("runSyncCheck"):
+    if (runType == 0) {
         Serial.println("Begin Synchronization");
         syncCheck();
-        break;
-    case("runMotorClockwise"):
+    } else if (runType == 1) {
         Serial.println("Running Clockwise");
         runMotorClockwise();
-        break;
-    case("runMotorAntiClockwise"):
+    } else if (runType == 2) {
         Serial.println("Running Anti-Clockwise");
         runMotorAntiClockwise();
-        break;
-    default:
+    } else {
         Serial.println("Choose a mode");
-        break;
     }
 }
 
@@ -73,35 +66,60 @@ void runMotorAntiClockwise() {
     digitalWrite(motorDriverPin2, HIGH);
 }
 
+void haltMotor() {
+    digitalWrite(motorDriverPin1, LOW);
+    digitalWrite(motorDriverPin2, LOW);  
+}
 
 void syncCheck() {
-    long int time = millis();
-    bool direction = true;
-    int prevTime = 0;
+    long prev1 = 0;
+    long prev2 = 0;
     int endTime = 10000;
 
     //infinite loop
     while(true) {
-        while(time - prev1 <= endTime) {
-            if(direction) {runMotorClockwise();} 
-            else {runMotorAntiClockwise();}
-            if(time - prev2 <= 1000) { //every second check
+        while(millis() - prev1 <= endTime) {
+            runMotorClockwise();
+            if(millis() - prev2 <= 1000) {
                 noInterrupts();
                 unsigned long pulses = pulseCount;
                 pulseCount = 0;
                 interrupts();
                 float revolutions = (float) pulses / pulsesPerRevolution;
+                float rpm = revolutions * 60.0;
                 totalRotations += revolutions; 
-                prev2 = time;
+                Serial.print("Live RPM: ");
+                Serial.print(rpm, 2);
+                prev2 = millis();
             } 
         }
-        Serial.println("Opening sequence of " + time - prev1/1000 + " seconds done in " + totalRotations + " revolutions.");
-        totalRotations = 0;
-        direction = !direction
-        prev1 = time;       
 
-        //has to be a different part completely for reverse as we are counting down
-        //the revolutions - this will be super close to the actual procedure.
-        endTime += 10000;
+        haltMotor();
+        Serial.print("Opening sequence of ");
+        Serial.print((millis() - prev1) / 1000);
+        Serial.print(" seconds done in ");
+        Serial.print(totalRotations);
+        Serial.println(" revolutions.");
+        totalRotations = 0;
+        prev1 = millis();   
+        delay(2000);
+
+        Serial.println("Lowering Bridge back to neutral state.");
+        Serial.println("Check for consistency alignment.");
+
+        //this part will be similar to new method for main
+        float reverseRevolutions = 0.0;
+        while(reverseRevolutions <= totalRotations) {
+            runMotorAntiClockwise();
+            noInterrupts();
+            unsigned long pulses = pulseCount;
+            pulseCount = 0;
+            interrupts();
+            reverseRevolutions += (float) pulses / pulsesPerRevolution;
+        }
+
+        haltMotor();
+        totalRotations = 0;
+        endTime += 10000; //add 10 secs to opening
     }
 }
